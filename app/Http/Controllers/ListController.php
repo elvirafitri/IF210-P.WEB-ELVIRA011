6633ac81503846b0b5b6d9d5cddf766c037047b1
elvirafitri <elvirafitriayu@gmail.com>
2024-03-18 17:21:57 +0700

    Praktikum 6

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListController extends Controller
+{
+    public function getData(){
+        $data = [
+            ["id" => 1, "produk"=> "Kaktus"],
+            ["id" => 2, "produk"=> "Bulgenvile"],
+            ["id" => 3, "produk"=> "Rose"],
+            ["id" => 4, "produk"=> "Matahari"],
+        ];
+        return $data;
+    }
+
+    public function index(){
+        $data = $this->getData();
+
+        return view("list_product", compact("data"));
+    }
+}
